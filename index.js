097578cc0a105e0d8c8b14cf8de8ab0a43a4de08
tkidanu <123982441+tkidanu@users.noreply.github.com>
2023-03-06 10:50:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-/**
- * @typedef {import('./lib/react-markdown.js').ReactMarkdownOptions} Options
- * @typedef {import('./lib/ast-to-react.js').Components} Components
- */
 
 export {uriTransformer} from './lib/uri-transformer.js'
 
